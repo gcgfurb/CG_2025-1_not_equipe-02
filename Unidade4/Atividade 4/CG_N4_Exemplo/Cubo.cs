04dfcfa3c2a420ffeb0be7b495250c3281910b00
Luiz Martendal <luiz.martendal52@gmail.com>
2025-06-04 19:29:05 -0300

    adicionando correção do cubo

--- Unidade4/Atividade 4/CG_N4_Exemplo/Cubo.cs
+++ Unidade4/Atividade 4/CG_N4_Exemplo/Cubo.cs
@@ -17,7 +17,7 @@ namespace gcgcg
 
     public Cubo(Objeto _paiRef, ref char _rotulo) : base(_paiRef, ref _rotulo)
     {
-      PrimitivaTipo = PrimitiveType.TriangleFan;
+      PrimitivaTipo = PrimitiveType.Triangles;
       PrimitivaTamanho = 10;
 
       vertices = new Ponto4D[]
@@ -32,41 +32,47 @@ namespace gcgcg
         new Ponto4D(-1.0f,  1.0f, -1.0f)
       };
 
-      // // 0, 1, 2, 3 Face da frente
       base.PontosAdicionar(vertices[0]);
       base.PontosAdicionar(vertices[1]);
+      base.PontosAdicionar(vertices[2]);
+      base.PontosAdicionar(vertices[0]);
       base.PontosAdicionar(vertices[2]);
       base.PontosAdicionar(vertices[3]);
 
-      // // 3, 2, 6, 7 Face de cima
+      base.PontosAdicionar(vertices[4]);
+      base.PontosAdicionar(vertices[7]);
+      base.PontosAdicionar(vertices[6]);
+      base.PontosAdicionar(vertices[4]);
+      base.PontosAdicionar(vertices[6]);
+      base.PontosAdicionar(vertices[5]);
+
+      base.PontosAdicionar(vertices[4]);
+      base.PontosAdicionar(vertices[5]);
+      base.PontosAdicionar(vertices[0]);
+      base.PontosAdicionar(vertices[0]);
+      base.PontosAdicionar(vertices[5]);
+      base.PontosAdicionar(vertices[1]);
+
       base.PontosAdicionar(vertices[3]);
       base.PontosAdicionar(vertices[7]);
+      base.PontosAdicionar(vertices[4]);
+      base.PontosAdicionar(vertices[4]);
+      base.PontosAdicionar(vertices[0]);
+      base.PontosAdicionar(vertices[3]);
+
+      base.PontosAdicionar(vertices[1]);
+      base.PontosAdicionar(vertices[5]);
+      base.PontosAdicionar(vertices[2]);
       base.PontosAdicionar(vertices[6]);
       base.PontosAdicionar(vertices[2]);
-      
-      // // 4, 7, 6, 5 Face do fundo
-      base.PontosAdicionar(vertices[4]);
+      base.PontosAdicionar(vertices[5]);
+
+      base.PontosAdicionar(vertices[3]);
+      base.PontosAdicionar(vertices[2]);
+      base.PontosAdicionar(vertices[6]);
       base.PontosAdicionar(vertices[7]);
+      base.PontosAdicionar(vertices[3]);
       base.PontosAdicionar(vertices[6]);
-      base.PontosAdicionar(vertices[5]);
-      
-      // // 0, 3, 7, 4 Face esquerda
-      base.PontosAdicionar(vertices[0]);
-      base.PontosAdicionar(vertices[4]);
-      base.PontosAdicionar(vertices[5]);
-      base.PontosAdicionar(vertices[1]);
-
-      // // 0, 4, 5, 1 Face de baixo
-      base.PontosAdicionar(vertices[0]);
-      base.PontosAdicionar(vertices[4]);
-      base.PontosAdicionar(vertices[5]);
-      base.PontosAdicionar(vertices[1]);
-
-      // // 1, 5, 6, 2 Face direita
-      base.PontosAdicionar(vertices[5]);
-      base.PontosAdicionar(vertices[6]);
-      base.PontosAdicionar(vertices[2]);
-      base.PontosAdicionar(vertices[1]);
 
       Atualizar();
     }
